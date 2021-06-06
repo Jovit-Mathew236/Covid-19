e82737de5499c9a404f57136f66335969efc8631
Jovit-Mathew236 <jovitmathew236@gmail.com>
2021-06-07 00:14:29 +0530

    covid 19 day7

--- index.js
+++ index.js
@@ -14,3 +14,34 @@ const myFunction = () => {
   }
 };
 
+// var requestOptions = {
+//   method: 'GET',
+//   redirect: 'follow'
+// };
+
+// fetch("https://api.covid19api.com/summary", requestOptions)
+//   .then(res => res.json())
+//   .then(result => console.log(result))
+//   .catch(error => console.log('error', error));
+
+
+
+$(document).ready(function(){
+  init()
+
+  function init(){
+    var url = "https://api.covid19api.com/summary"
+    var data = ''
+    $.get(url,function(data){
+      console.log(data);
+
+      data =`
+      <td>${data.Global.TotalDeaths}</td>
+      <td>${data.Global.TotalRecovered}</td>
+      <td>${data.Global.TotalConfirmed}</td>
+      `
+
+      $("#data").html(data)
+    })
+  }
+})
