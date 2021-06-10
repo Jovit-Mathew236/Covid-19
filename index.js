54aed2c181ca8d57b31ee19ce9318d36dc83c33f
Jovit-Mathew236 <jovitmathew236@gmail.com>
2021-06-10 10:36:41 +0530

    covid vaccine details add

--- index.js
+++ index.js
@@ -69,6 +69,12 @@ $(document).ready(function () {
   }
 });
 
+
+
+var date = new Date();
+var today = date.getDate() + "-" + (date.getMonth()+1) + "-" + date.getFullYear();
+
+
 $(document).ready(function () {
   init2();
 
@@ -120,22 +126,98 @@ $(document).ready(function () {
 
           selectDistrict.addEventListener("change", function (e) {
             if (e.target.value == singleDistrict.district_name) {
-              console.log(singleDistrict.district_name);
-              //callDistrictId(singleDistrict.district_id);
+              //console.log(singleDistrict.district_name);
+              callDistrictId(singleDistrict.district_id);
             }
           });
         }
       }
 
       function makeNewOptionBoxDistrict(data) {
-        console.log(data);
+        //console.log(data);
         const districtName = data.district_name;
         if (typeof districtName != "undefined") {
           const optionBoxDistrict = document.createElement("option");
           optionBoxDistrict.innerHTML = districtName;
           selectDistrict.appendChild(optionBoxDistrict);
+          return;
         }
       }
+
+      function callDistrictId(districtData) {
+        var url =
+          "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id="+districtData+"&date="+today;
+        $.get(url, function (data) {
+          //console.log(data);
+          manupulateData1(data);
+        });
+      }
+
+      const selectCenter = document.querySelector("select#center");
+
+      function manupulateData1(data) {
+        for (eachCenter in data.sessions) {
+          const singleCenter = data.sessions[eachCenter];
+          //console.log(singleCenter);
+          makeNewOptionBoxCenter(singleCenter);
+
+          selectCenter.addEventListener("change", function (e) {
+            if (e.target.value == singleCenter.name) {
+              //console.log(singleCenter.address);
+              // callCenterId(singleCenter);
+              
+        const result =`
+      <p>${'address:'+singleCenter.address}</p>
+      <p>${'address:'+singleCenter.block_name}</p>
+      <p>${'address:'+singleCenter.available_capacity_dose1}</p>
+      <p>${'address:'+singleCenter.available_capacity_dose2}</p>
+      <p>${'address:'+singleCenter.addres}</p>
+      <p>${'address:'+singleCenter.addres}</p>
+      <p>${'address:'+singleCenter.addres}</p>
+      <p>${'address:'+singleCenter.addres}</p>
+      <p>${'address:'+singleCenter.addres}</p>
+      <p>${'address:'+singleCenter.addres}</p>
+      <p>${'address:'+singleCenter.addres}</p>
+
+      `;
+      $("div#result").html(result)
+            }
+          });
+        }
+      }
+
+      function makeNewOptionBoxCenter(data) {
+        //console.log(data);
+        const centerName = data.name;
+        if (typeof centerName != "undefined") {
+          const optionBoxCenter = document.createElement("option");
+          optionBoxCenter.innerHTML = centerName;
+          selectCenter.appendChild(optionBoxCenter);
+        }
+      }
+
+
+      // function callCenterId(centerData){
+      //   console.log(centerData);
+        
+      // }
     });
   }
 });
+
+
+// address: "Vizhickathodu PHCVizhickathodu PO Kanjirappally Kottayam"
+// available_capacity: 0
+// available_capacity_dose1: 0
+// available_capacity_dose2: 0
+// block_name: "Erumely CHC"
+// center_id: 127701
+// date: "10-06-2021"
+// fee: "0"
+// fee_type: "Free"
+// from: "09:00:00"
+// min_age_limit: 18
+// pincode: 686518
+// slots: (4) ["09:00AM-10:00AM", "10:00AM-11:00AM", "11:00AM-12:00PM", "12:00PM-02:00PM"]
+// to: "14:00:00"
+// vaccine: "COVISHIELD"
